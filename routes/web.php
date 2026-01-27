39ab5648caf317af363cfc93a1f3542bb49fb846
Alan7771-on <alan.buretic@ss-adamic.com>
2026-01-27 14:06:38 +0100

    htcfo

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Početna stranica
 Route::get('/', function () {
-    return view('hrana');
-
-Route::get('/cevapi-restorani', function () {
-    return view('');
+    return view('hrana'); // naziv Blade fajla bez .blade.php
 });
 
+// Stranica sa ćevapima
+Route::get('/cevapi-restorani', function () {
+    return view('cevapi-restorani'); // ovde stavi ime odgovarajućeg Blade fajla
+});
 
-
+// Stranica sa crnim rižotom
+Route::get('/crni-rizot-restorani', function () {
+    return view('crni-rizot-restorani'); // ovde stavi ime Blade fajla za crni rižot
 });
 
 
