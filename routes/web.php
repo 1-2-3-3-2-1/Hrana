aa22fc53fc3891262b69134eab975b759915c492
Alan7771-on <alan.buretic@ss-adamic.com>
2026-02-03 13:42:42 +0100

    ALAN JE GENIJE

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,49 @@ Route::get('/crni-rizot-restorani', function () {
 });
 
 
+
+// Stranica sa brodetom
+Route::get('/brodet-restorani', function () {
+    return view('brodet-restorani'); // ovde stavi ime odgovarajućeg Blade fajla
+});
+
+// Stranica sa pasticadom
+Route::get('/pasticada-restorani', function () {
+    return view('pasticada-restorani'); // ovde stavi ime Blade fajla za pasticadu
+});
+
+// Stranica sa pečenjem
+Route::get('/peka-restorani', function () {
+    return view('peka-restorani'); // ovde stavi ime odgovarajućeg Blade fajla
+});
+
+// Stranica sa punjenom paprikom
+Route::get('/punjena-paprika-restorani', function () {
+    return view('punjena-paprika-restorani'); // ovde stavi ime Blade fajla za punjenu papriku
+});
+
+// Stranica sa sarma
+Route::get('/sarma-restorani', function () {
+    return view('sarma-restorani'); // ovde stavi ime odgovarajućeg Blade fajla
+});
+
+// Stranica sa soparnikom
+Route::get('/soparnik-restorani', function () {
+    return view('soparnik-restorani'); // ovde stavi ime Blade fajla za soparnik
+});
+
+// Stranica sa brodetom
+Route::get('/o-nama', function () {
+    return view('o-nama'); // ovde stavi ime odgovarajućeg Blade fajla
+});
+
+// Stranica sa pasticadom
+Route::get('/login', function () {
+    return view('login'); // ovde stavi ime Blade fajla za login
+});
+
+// Stranica sa brodetom
+Route::get('/kontakt', function () {
+    return view('kontakt'); // ovde stavi ime odgovarajućeg Blade fajla
+});
+
